f465230d3fcf1f7ed543a2b1139707639bffb6f7
unknown <dannyalmighty@icloud.com>
2015-10-08 16:32:57 -0500

    IF class for specials done i think.  I might be off with spacing. I havent tested it yet

--- Special/If.cs
+++ Special/If.cs
@@ -13,7 +13,35 @@ namespace Tree
 
         public override void print(Node t, int n, bool p)
         {
-            // TODO: Implement this function.
+            for (int i = 0; i < n; i++)
+                Console.Write(" ");
+            //Write IF from the start.  Once the expressions passes the else cause, Close with ")"
+            Console.Write("(if");
+
+            //Define the cond after if
+            if (t.getCdr().getCar().isPair())
+            {
+                Node iff = t.getCdr().getCar();
+                iff.print(0, p);
+            }
+            else Console.WriteLine();
+
+            //Define the then statement
+            Node thenn = t.getCdr().getCdr().getCar();
+            if(!thenn.isNull())
+            {
+                thenn.print(n + 2, p);
+            }
+            else Console.WriteLine();
+
+            //Goes down the scheme until gets to the else statement
+            Node elsee = t.getCdr().getCdr().getCdr().getCar();
+            while(!elsee.isNull())
+            {
+                elsee.print(n + 2, p);
+            }
+  
+            Console.WriteLine(")");          
         }
     }
 }
